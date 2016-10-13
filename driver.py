da47b457deea11dc7de33c7d02d21e5a295bbdc5
Matthew <mbrimmer1@gmail.com>
2016-10-13 12:59:04 -0400

    Final version fixed linkedin education and location bugs

--- driver.py
+++ driver.py
@@ -1,9 +1,11 @@
+import os
+import sys
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+# I was working inside a python virtual env during this project
 # Create a new instance of the Firefox driver. I spent a good deal of time trouble shooting Firefox driver problems and was finally able to make it work by using an older version of FireFox(46.0.1).
 driver = webdriver.Firefox()
 
@@ -74,7 +76,7 @@ finally:
 
             return leader_name_count
 
-        def getLinkedInInformation(self, url):
+        def getLinkedInInformation(self, url, logged_in):
             for child in self.parent.find_elements_by_tag_name("a"):
                 if child.get_attribute("href") == url:
                     link = child
@@ -82,23 +84,45 @@ finally:
             driver.execute_script("arguments[0].click();", link)
             for handle in driver.window_handles:
                 driver.switch_to.window(handle)
+            # I ran into a problem when selenium click the linkedIn link for the first time, I was not logged in to LinkedIn. I would then log in and switch the logged_in variable to True so that I would not repeat the process after the initial log in. The instuctions said to click the links. Another solution would be to follow each link that I have stored so selenium isnt opening new browsers for each link. To test this, a username and password will need to be entered as I do not wish to give out my password for linkedin.
+            if logged_in == False:
+                try:
+                    element = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, "sign-in-link")))
+                finally:
+                    link = driver.find_element_by_link_text("Sign in")
+                    link.click()
+                try:
+                    element = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.ID, "btn-primary")))
+                finally:
+                    email_input = driver.find_element_by_name("session_key")
+                    # Input linkedin email address to test this.
+                    email_input.send_keys('email)
+                    password_input = driver.find_element_by_name("session_password")
+                    # Input linkedin pasword to test this.
+                    password_input.send_keys('password')
+                    submit = driver.find_element_by_name('signin')
+                    submit.click()
             try:
-                element = WebDriverWait(driver, 30).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "locality")))
+                element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, "full-name")))
             finally:
-                for location in driver.find_elements_by_css_selector("span.locality"):
-                    print element.text
-                    local = element.text
+                for location in driver.find_elements_by_xpath("//*[@id='location']/dl/dd[1]/span/a"):
+                    local = location.text
+                for education in driver.find_elements_by_xpath("//*[@id='overview-summary-education']/td/ol/li/a"):
+                    edu = education.text
                 driver.switch_to.window(driver.window_handles[0])
-                return local
+                return [local, edu]
+
         # Class method that calls the other methods and return a dictionary for leader at given location
-        def buildLeaderDictionary(self):
+        def buildLeaderDictionary(self, logged_in):
             name = self.getBetterCloudName()
             title = self.getBetterCloudTitle()
             image = self.getBetterCloudLeaderImage()
             Linkedin = self.getBetterCloudLinkedIn()
             count = self.getBetterCloudNameCount(name)
-            linked_in_info = self.getLinkedInInformation(Linkedin)
+            linked_in_info = self.getLinkedInInformation(Linkedin, logged_in)
             split_name = name.split()
             firstname = split_name[0]
             lastname = split_name[1]
@@ -109,15 +133,39 @@ finally:
             'image_url': image,
             'linkedin': Linkedin,
             'name_count': count,
-            'location': linked_in_info
+            'location': linked_in_info[0],
+            'education': linked_in_info[1]
             }
             return new_dict
 
 
-    leader_location = ["//*[@id='leaders']/div/div[2]/div[1]","//*[@id='leaders']/div/div[2]/div[2]","//*[@id='leaders']/div/div[2]/div[3]","//*[@id='leaders']/div/div[2]/div[4]","//*[@id='leaders']/div/div[2]/div[5]","//*[@id='leaders']/div/div[2]/div[6]","//*[@id='leaders']/div/div[2]/div[7]"]
     leaders = []
-    for leader in leader_location:
-        the_leader = Leader(leader).buildLeaderDictionary()
-        leaders.append(the_leader)
-        print the_leader
+    def getLeaders():
+        # Xpath location of each leader on the bettercloud about us web page.
+        leader_location = ["//*[@id='leaders']/div/div[2]/div[1]","//*[@id='leaders']/div/div[2]/div[2]","//*[@id='leaders']/div/div[2]/div[3]","//*[@id='leaders']/div/div[2]/div[4]","//*[@id='leaders']/div/div[2]/div[5]","//*[@id='leaders']/div/div[2]/div[6]","//*[@id='leaders']/div/div[2]/div[7]"]
+
+        # The logged_in variable is passed when the buildLeaderDictionary function is called so that the log in process is only attempted once.
+        logged_in = False
+        for leader in leader_location:
+            the_leader = Leader(leader).buildLeaderDictionary(logged_in)
+            leaders.append(the_leader)
+            logged_in = True
+            print the_leader
+    # Writes data to the csv file leaders.csv
+    def writeLeaderData():
+        with open('leaders.csv', "wb") as csvfile:
+            write = csv.writer(csvfile, delimiter = ' ')
+            for leader in leaders:
+                write.writerow(["Title:", leader['title']])
+                write.writerow(["First Name:", leader['firstname']])
+                write.writerow(["Last Name:", leader['lastname']])
+                write.writerow(["Location:", leader['location']])
+                write.writerow(["Education:", leader['education']])
+                write.writerow(["Name count:", leader['name_count']])
+                write.writerow(["LinkedIn:", leader['linkedin']])
+                write.writerow(["Image url:", leader['image_url']])
+                write.writerow(["# - - - - - - - - - - - - - - #"])
+
+    getLeaders()
+    writeLeaderData()
     driver.quit()
